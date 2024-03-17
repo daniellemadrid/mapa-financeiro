511cb453bcd90c9548840bad22c584e1e4ddfc24
Gustavo Machado <gustavo.lopes04@edu.pucrs.br>
2024-03-16 22:01:57 -0300

    Primeira classe do projeto

---
+++ mapa.java
@@ -0,0 +1,35 @@
+package mapa_financeiro;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class mapa {
+    private Integer orientacao;
+    private Boolean invertido;
+    private String[][] mapaStrings;
+    private String nomeArquivo = "arquivo.txt";
+    private int contagem;
+
+    public static int contarColunasNaoNulas(String nomeArquivo) {
+        int contagemColunas = 0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(nomeArquivo))) {
+            String linha;
+
+            while ((linha = br.readLine()) != null) {
+                String[] colunas = linha.split("\t"); // Assumindo que as colunas são separadas por tabulação (\t)
+
+                for (String coluna : colunas) {
+                    if (!coluna.trim().isEmpty()) { // Verifica se a coluna não está vazia
+                        contagemColunas++;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Erro ao ler o arquivo: " + e.getMessage());
+        }
+
+        return contagemColunas;
+    }
+}
